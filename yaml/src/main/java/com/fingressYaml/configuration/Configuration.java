72ad623bd8fa0a848b7a8d927ec48f00ab3d208b
YogeshVicky <149947822+YogeshVicky@users.noreply.github.com>
2024-07-01 10:51:08 +0530

    Add files via upload
    FingressYaml

---
+++ yaml/src/main/java/com/fingressYaml/configuration/Configuration.java
@@ -0,0 +1,42 @@
+package com.fingressYaml.configuration;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.component.google.sheets.GoogleSheetsComponent;
+import org.apache.camel.component.google.sheets.GoogleSheetsConfiguration;
+import org.apache.camel.spring.boot.CamelContextConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@org.springframework.context.annotation.Configuration
+public class Configuration {
+
+	@Bean
+	public HikariDataSource myDataSource() {
+
+		HikariConfig config = new HikariConfig();
+		config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		config.setJdbcUrl("jdbc:mysql://localhost:3307/employee");
+		config.setUsername("root");
+		config.setPassword("root");
+		return new HikariDataSource(config);
+	}
+
+	@Bean
+	public static CamelContextConfiguration contextConfiguration(HikariDataSource myDataSource) {
+		return new CamelContextConfiguration() {
+
+			@Override
+			public void beforeApplicationStart(CamelContext context) {
+				context.getRegistry().bind("myDataSource", myDataSource);
+			}
+
+			@Override
+			public void afterApplicationStart(CamelContext camelContext) {
+
+			}
+		};
+	}
+
+}
